166a5245e7773f8e35289926b4bd6087b713e371
Abe Sharp <abe@datadoc.info>
2025-01-26 20:27:11 -0800

    host role init scripts

--- rag-example/create-rag-example.sh
+++ rag-example/create-rag-example.sh
@@ -3,28 +3,36 @@
 if [ -z $DO_PAT ];then read -p "Enter Digital Ocean Personal Access Token: " do_pat;export DO_PAT=$do_pat;fi
 if [ -z $NGC_API_KEY ];then read -p "Enter Nvidia NGC API Key: " ngc_api_key;export NGC_API_KEY=$ngc_api_key;fi
 if [ -z $DO_SSH_KEYFILE ];then read -p "Enter path to private key for Digital Ocean droplets: " do_ssh_keyfile;export DO_SSH_KEYFILE=$do_ssh_keyfile;fi
+
 DOMAIN="quyver.com"
 export LLMHOSTNAME="llmhost"
 export EMBEDDINGHOSTNAME="embeddinghost"
 
 tofu init
-tofu plan -var "do_token=${DO_PAT}" -var "pvt_key=${DO_SSH_KEYFILE}" \
-    -var "ngc_api_key=${NGC_API_KEY}" -var "gpuhost_names=[\"$LLMHOSTNAME\",\"$EMBEDDINGHOSTNAME\"]"
-tofu apply -var "do_token=${DO_PAT}" -var "pvt_key=${DO_SSH_KEYFILE}" \
-    -var "ngc_api_key=${NGC_API_KEY}" -var "gpuhost_names=[\"$LLMHOSTNAME\",\"$EMBEDDINGHOSTNAME\"]" -auto-approve
-tofu output -json |jq -r '.host_info.value | to_entries[]| "\(.value) \(.key)"'>outputs.txt
 
+tofu plan -var "do_token=${DO_PAT}" -var "pvt_key=${DO_SSH_KEYFILE}" -var "ngc_api_key=${NGC_API_KEY}" -var "gpuhost_names=[\"$LLMHOSTNAME\",\"$EMBEDDINGHOSTNAME\"]"
+
+tofu apply -var "do_token=${DO_PAT}" -var "pvt_key=${DO_SSH_KEYFILE}" -var "ngc_api_key=${NGC_API_KEY}" -var "gpuhost_names=[\"$LLMHOSTNAME\",\"$EMBEDDINGHOSTNAME\"]" -auto-approve
+
+tofu output -json |jq -r '.host_info.value | to_entries[]| "\(.value),\(.key)"' > outputs.txt
+
+LLMHOSTIP=$(cat outputs.txt|grep $LLMHOSTNAME|cut -f 1 -d ",")
+EMBEDDINGHOSTIP=$(cat outputs.txt|grep $EMBEDDINGHOSTNAME|cut -f 1 -d ",")
+
+echo "Got LLM Host IP of $LLMHOSTIP"
+echo "Got Embedding Host IP of $EMBEDDINGHOSTIP"
+
+#remove old key fingerprints for dns names
 ssh-keygen -f "/home/abe/.ssh/known_hosts" -R "$EMBEDDINGHOSTNAME.quyver.com"
 ssh-keygen -f "/home/abe/.ssh/known_hosts" -R "$LLMHOSTNAME.quyver.com"
 
-#wait for DNS entries to propagate
-sleep 10
-
 #copy the init scripte to each new host
-scp -i $DO_SSH_KEYFILE ./init.sh root@$LLMHOSTNAME.$DOMAIN:~
-scp -i $DO_SSH_KEYFILE ./init.sh root@$EMBEDDINGHOSTNAME.$DOMAIN:~
+scp -i $DO_SSH_KEYFILE ./init-llm.sh root@$LLMHOSTIP:~
+scp -i $DO_SSH_KEYFILE init-embedding.sh root@$EMBEDDINGHOSTIP:~
 #run the init scripts
+ssh -i $DO_SSH_KEYFILE root@$LLMHOSTIP "chmod o+x ./init-llm.sh && ./init-llm.sh" &
+ssh -i $DO_SSH_KEYFILE root@$EMBEDDINGHOSTIP "chmod o+x ./init-embedding.sh && ./init-embedding.sh" &
+wait
 
 #print out the LLM and Embedding endpoints for use in Python script
-
-echo "Complete. Before accessing the inferencing endpoint, wait 5 minutes for the NIM container to fully start up"
+echo "Complete. Before accessing the inferencing endpoints, wait 5 minutes for the NIM containers to fully start up"
--- rag-example/init-llm.sh
+++ rag-example/init-llm.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -x
+echo "Running llm init script"
+
 export PATH=$PATH:/usr/bin
 export DEBIAN_FRONTEND=noninteractive
+export NGC_API_KEY=$(cat /root/ngc-api-key)
+
 apt-get update
 sleep 30
 apt-get install -yq apt-transport-https ca-certificates curl gpg lsb-release unzip
+
 curl -LO https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz
 tar Cxzvvf /usr/local nerdctl-full-1.7.6-linux-amd64.tar.gz
 echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.d/custom.conf
@@ -12,10 +18,11 @@ systemctl start containerd
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 apt-get update && apt-get install -yq nvidia-container-toolkit
 nerdctl run --rm --gpus all ubuntu nvidia-smi
+
 echo $NGC_API_KEY | docker login nvcr.io --username '$oauthtoken' --password-stdin
 curl -LO https://api.ngc.nvidia.com/v2/resources/nvidia/ngc-apps/ngc_cli/versions/3.54.0/files/ngccli_linux.zip
 unzip ngccli_linux.zip && chmod u+x ngc-cli/ngc && rm -rf ./ngccli_linux.zip
 mkdir -p ~/.cache/nim
 iptables -A CNI-FORWARD -p tcp -m tcp --dport 8000 -j ACCEPT
-nerdctl run -d --gpus='"device=0"'--shm-size=16GB --name llm -e NGC_API_KEY -v ~/.cache/nim:/opt/nim/.cache -u 0 -p 8000:8000 nvcr.io/nim/meta/llama3-8b-instruct:1.0.0
-nerdctl run -d --gpus='"device=0"'--shm-size=16GB --name embedding -e NGC_API_KEY -v ~/.cache/nim:/opt/nim/.cache -u 0 -p 8000:8000 nvcr.io/nim/nvidia/nv-embedqa-e5-v5:latest
+
+echo "COMMAND: nerdctl run -d --gpus='\"device=0\"'--shm-size=16GB --name llm -e NGC_API_KEY -v ~/.cache/nim:/opt/nim/.cache -u 0 -p 8000:8000 nvcr.io/nim/meta/llama3-8b-instruct:1.0.0"
